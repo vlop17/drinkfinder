48f72f94506118d0f5ad276b6e32fd86877b3e1a
vlop17 <100816570+vlop17@users.noreply.github.com>
2022-03-02 20:33:23 -0600

    Create body.py

---
+++ body.py
@@ -0,0 +1,14 @@
+# project idea for skill showoff
+# make a drink reccomendor
+# input liquor type and flavor profile and will return drink reccomendation from library it has
+# will need to have dictionary with many entries in order to provide many possibilities
+# typing random will make any drink show up
+#  leaving it null will pop up question asking if passes out
+# any wrong entry will ask user if they are drunk and prompt re entry of choice
+# if dont have selection say woops you've selected something we dont know
+
+def what_to_drink(name,liqour,flavor):
+  print("Hi "+ name+"! I hear you like " + flavor + " " + liqour + " kinds of drinks")
+
+
+what_to_drink("Vic","vodka","sweet")
